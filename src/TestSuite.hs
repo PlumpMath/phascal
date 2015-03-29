e52c89c0755d06bc940767e005cc32ded48c3d46
Ian Denhardt <ian@zenhack.net>
2015-03-29 03:16:15 -0400

    Move test suite to its own module, clean up Main
    We now have a more sensible way of determining what to do on startup.
    Before, we ran the tests, and if there were any failures, report them.
    If not, parse and print the AST for stdin. This was pretty gross; now
    you can run the tests by passing "test" as the first argument.

---
+++ src/TestSuite.hs
@@ -0,0 +1,72 @@
+module TestSuite (runTests) where
+
+import Ast
+import Parser
+import Text.ParserCombinators.Parsec (ParseError)
+
+tests = [ ("ex1",
+          [ Program "TheSource" [] []
+              [ Assign "a" (Var "b3")
+              , Assign "xyz" (Op Minus (Op Plus (Op Plus (Var "a")
+                                                         (Op Mod (Var "b")
+                                                                 (Var "c")))
+                                                (Var "c"))
+                                       (Op Div (Var "p")
+                                               (Var "q")))
+              , Assign "a" (Op Times (Var "xyz") (Op Plus (Var "p") (Var "q")))
+              , Assign "p" (Op Minus (Op Minus (Var "a") (Var "xyz")) (Var "p"))
+              ]
+          ])
+        , ("ex2",
+          [ Program "decisions" ["input", "output"] [ (["i", "j"], TyInt)
+                                                    , (["variable"], TyBool)
+                                                    ]
+            [ If (Op Gt (Var "i") (Var "j"))
+                 (Assign "i" (Op Plus (Var "i") (Var "j")))
+                 (Just $ If (Var "variable")
+                         (If (Op LtEq (Var "i") (Var "j"))
+                             (Assign "i" (Num 0))
+                             (Just $ Assign "j" (Num 0)))
+                         Nothing)
+            ]
+          ])
+        , ("ex3",
+          [ Program "HasLoops" ["input", "output"] [ (["i", "j"], TyInt)
+                                                   , (["NotDone"], TyBool)
+                                                   ]
+            [ While (Op And (Op Lt (Var "i") (Var "j")) (Var "NotDone"))
+               (CompoundStatement [ Assign "k" (Op Plus (Var "k") (Num 1))
+                                  , While (Op Eq (Var "i") (Var "j"))
+                                          (Assign "i" (Op Plus (Var "i") (Num 2)))
+                                  ])
+            ]
+          ])
+        , ("ex4",
+          [ Program "signsAndBools" [] []
+            [ If (Not T)
+                 (Assign "x" (Op Plus (Num 4) (Neg (Num 2))))
+                 (Just (Assign "y" F))
+            ]
+          ])
+        ]
+
+type TestFailure = Either ParseError ([Program], [Program])
+type TestResult = Either TestFailure [Program]
+
+runTest :: String ->  String -> [Program] -> TestResult
+runTest name input ast = case parse name input of
+    Left e -> Left (Left e)
+    Right ast' ->
+        if ast' == ast
+        then Right ast'
+        else Left (Right (ast, ast))
+
+runTests :: IO [TestFailure]
+runTests = do
+    let filenames = ["ex/" ++ name ++ ".pscl" | name <- map fst tests]
+    files <- mapM readFile filenames
+    let testResults = zipWith3 runTest filenames files (map snd tests)
+    (return :: a -> IO a) $ map (\(Left e) -> e) $ flip filter testResults (\res ->
+        case res of
+            Left _ -> True
+            Right _ -> False)
