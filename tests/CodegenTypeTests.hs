c53d3b4f57e828d5fc1426304c58d814748aa1f8
Ian Denhardt <ian@zenhack.net>
2015-06-18 01:34:00 -0400

    Add quickcheck test
    Property: anything which typechecks should successfully generate code.
    
    We don't actually check for things with variable names; will get to that
    eventually.
    
    The above is failing, since some of the codegen is unimplemented -- as
    noted in the source, booleans do not currently pass.

---
+++ tests/CodegenTypeTests.hs
@@ -0,0 +1,49 @@
+module CodegenTypeTests where
+
+import Phascal.TypeChecker
+import Phascal.Arm
+import Phascal.Ast
+import Phascal.SymbolTable (SymTable, empty)
+
+import Test.QuickCheck.Arbitrary
+import Test.QuickCheck.Gen
+
+instance Arbitrary Expr where
+    arbitrary = oneof [ Num <$> arbitrary
+--                      , Var <$> arbitrary -- Random variable names are
+--                                          -- something I'm not quite ready to
+--                                          -- deal with
+                      , Op  <$> arbitrary <*> arbitrary <*> arbitrary
+                      , Not <$> arbitrary
+                      , return T
+                      , return F
+                      , Neg <$> arbitrary
+                      , Pos <$> arbitrary
+                      ]
+
+instance Arbitrary SymTable where
+    arbitrary = return empty
+
+instance Arbitrary BinOp where
+    arbitrary = oneof (map return [ Plus
+                                  , Minus
+                                  , Times
+                                  , Div
+                                  , Mod
+                                  , And
+                                  , Or
+                                  , Eq
+                                  , NEq
+                                  , Lt
+                                  , LtEq
+                                  , Gt
+                                  , GtEq
+                                  ])
+
+-- Fails with expr: F (not implemented)
+ifTypeThenCode :: SymTable -> Expr -> Bool
+ifTypeThenCode syms expr = case (typeOf syms expr) of
+    Left _ -> True
+    Right _ -> case (compileExpr syms expr) of
+        Left _ -> False
+        Right _ -> True
--- tests/TestMain.hs
+++ tests/TestMain.hs
@@ -4,13 +4,17 @@ import ParserTests
 import ArmTests
 import TypeTests
 import ExecTests
+import CodegenTypeTests
 
 import Test.Framework
 import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck2
 
 main :: IO ()
 main = defaultMain [ testGroup "Parser tests" $ hUnitTestToTests parserTests
                    , testGroup "Arm tests"    $ hUnitTestToTests    armTests
                    , testGroup "Type tests"   $ hUnitTestToTests   typeTests
                    , testGroup "Exec Tests"   $ hUnitTestToTests   execTests
+
+                   , testProperty "If typechecks then generates code" ifTypeThenCode
                    ]
