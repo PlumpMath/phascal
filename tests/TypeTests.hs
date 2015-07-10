765d8be304e60dcf432f7ea49b0cca8e80a97c62
Ian Denhardt <ian@zenhack.net>
2015-07-09 21:15:43 -0400

    Add a couple tests re: typechecking
    These are passing, but I'm skeptical. There's a bug somewhere between
    here and the quickcheck tests, as some of these expressions seem to get
    past the typechecking there.

--- tests/TypeTests.hs
+++ tests/TypeTests.hs
@@ -1,6 +1,19 @@
 module TypeTests where
 
 import Test.HUnit
+import Phascal.Ast
+import Phascal.SymbolTable
 import Phascal.TypeChecker
 
-typeTests = TestList []
+typeTests = TestList
+                (map mktest [ (empty, Not (Num 0))
+                            , (empty, (Pos T))
+                            ])
+  where
+    mktest (syms, expr) = TestLabel "should-type-fail" (TestCase (assertTypeFail syms expr))
+
+assertTypeFail syms expr = case typeOf syms expr of
+    Left _ -> return ()
+    Right t -> failTest (show expr ++ "should not have typechecked, but got type " ++ show t)
+
+failTest msg = assertEqual msg 1 2
